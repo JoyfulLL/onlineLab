c564a43ec750b75ae4f4969fdbf6d40cfd6d3977
JoyfulLL <ljf13660117967@163.com>
2024-01-10 15:40:27 +0800

    新增用户按钮完善，注册功能需要重构，编辑、移除待写

---
+++ src/components/auth/studentRegistration.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+/**
+ * @file  studentRegistration
+ * @author ljf13
+ * @description
+ * @date 2024/1/10
+ */
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
