b07ef95adbb4966815f7694c9c47fb55edbe07e1
JoyfulLL <ljf13660117967@163.com>
2024-01-17 12:55:24 +0800

    优化学生管理

--- src/views/classManagement/studentManagement.vue
+++ src/views/classManagement/studentManagement.vue
@@ -4,22 +4,20 @@
  * @author ljf13
  * @description 用于学生管理——展示学生信息，注册新学生，修改学生信息
  * @date 2024/1/8
-  @@@ 代码结构
-  数据段data 建议新增数据都放在新增的方法前面
-  @避免出现无法找到数据的错误
-  方法段method
-  @当前已有功能：✔获取所有学生信息，✔新增学生，✔修改指定学生信息
-  @待做功能：
-  @ 1、将指定学生移出班级
-  @ 2、依据信息获取对应的学生
-  @ 3、✔页面的数据用pinia做状态管理，实现数据无感更新
+ @@@ 代码结构
+ 数据段data 建议新增数据都放在新增的方法前面
+ @避免出现无法找到数据的错误
+ 方法段method
+ @当前已有功能：✔获取所有学生信息，✔新增学生，✔修改指定学生信息
+ @待做功能：
+ @ 1、将指定学生移出班级
+ @ 2、依据信息获取对应的学生
+ @ 3、✔页面的数据用pinia做状态管理，实现数据无感更新
  */
-import {checkToken, getStuInfo, editStuInfo, regStu} from "@/api/manager";
-import {reactive, ref, onMounted, inject} from "vue";
-import {ElMessage} from "element-plus";
-import {usePaginationStore} from '@/stores/userData/storeUserData'
-
-
+import { checkToken, getStuInfo, editStuInfo, regStu } from "@/api/manager";
+import { reactive, ref, onMounted, inject } from "vue";
+import { ElMessage } from "element-plus";
+import { usePaginationStore } from "@/stores/userData/storeUserData";
 
 // @界面初始化，校验token合法后，再获取用户数据
 onMounted(() => {
@@ -29,7 +27,7 @@ onMounted(() => {
 
 // @注入APP.vue提供的刷新方法
 // @用于在新增用户/编辑用户后刷新表格
-const reload = inject('reload')
+const reload = inject("reload");
 
 // 展示数据用的表头
 const tableLabel = reactive([
@@ -90,11 +88,10 @@ const userForm = reactive({
 // 用于侦听数据的变化，使用newUserForm存放变化后的数据
 const newUserForm = reactive([]);
 watchEffect(() => {
-  newUserForm.value = {...userForm};
-  console.log(newUserForm)
+  newUserForm.value = { ...userForm };
+  console.log(newUserForm);
   // console.log('真名',newUserForm.value.realName)
 });
-
 
 const paginationStore = usePaginationStore();
 
@@ -108,7 +105,6 @@ const fetchData = async () => {
 const changePage = (pageNum) => {
   paginationStore.changePage(pageNum);
 };
-
 
 // @以下代码已弃用，已经用pinia存储了数据并且展示
 // ！！！
@@ -131,7 +127,6 @@ const changePage = (pageNum) => {
 //   return stuList.value.slice(startIndex, endIndex);
 // });
 
-
 // @以下代码用于 学生管理
 // @添加学生按钮——仅用于展示会话框
 // @编辑学生按钮——展示会话框以及获取到的表单数据
@@ -145,23 +140,22 @@ const dialogVisible = ref(false);
 const action = ref("add");
 
 // @显示密码框与否 编辑模式没有密码框
-const showPassword = ref()
+const showPassword = ref();
 
 // @用于在编辑模式禁用相关选项的修改
 // @目前除了realName，email，class外，全都禁用
-const IsDisabled = ref(false)
+const IsDisabled = ref(false);
 
 // @关闭会话框
 const handleClose = (done) => {
   ElMessageBox.confirm("确定关闭？")
-      .then(() => {
-        done();
-      })
-      .catch(() => {
-        // catch error
-      });
+    .then(() => {
+      done();
+    })
+    .catch(() => {
+      // catch error
+    });
 };
-
 
 //此函数仅用于调出会话框，并不是用于提交表单
 //提交表单的函数为handleSubmit
@@ -177,8 +171,7 @@ const addStudent = async () => {
   }
   showPassword.value = true;
   IsDisabled.value = false;
-}
-
+};
 
 //@用于”编辑按钮“，函数实际用途为查看用户信息
 //@将用户信息显示在表单中
@@ -189,11 +182,10 @@ const editStudent = (row) => {
   // console.log(row)
   nextTick(() => {
     Object.assign(userForm, row);
-  })
+  });
   showPassword.value = false;
   IsDisabled.value = true;
-}
-
+};
 
 // @此函数用于提交表单
 // @判断动作的值，为add则调用新增用户接口
@@ -201,155 +193,144 @@ const editStudent = (row) => {
 const handleSubmit = async () => {
   if (action.value === "add") {
     await regStu(
-        userForm.name,
-        userForm.password,
-        userForm.email,
-        userForm.realName,
-        userForm.userSchoollD,
-        userForm.schoolCode,
-        userForm.class,
-        userForm.sex
+      userForm.name,
+      userForm.password,
+      userForm.email,
+      userForm.realName,
+      userForm.userSchoollD,
+      userForm.schoolCode,
+      userForm.class,
+      userForm.sex,
     )
-        .then((res) => {
-          if (res.data.status === 0) {
-            //状态码为0，提交成功，关闭当前对话框
-            dialogVisible.value = false;
-            reload()
-            ElMessage({
-              message: '用户添加完毕',
-              type: 'success',
-            })
-          } 
-          }
-        })
-        .catch(e => {
-            let errorMessage = '登录失败';
-            switch (e.response.data.status) {
-              case 1:
-                errorMessage = '内部错误';
-                break;
-              case 2:
-                errorMessage = '登录令牌错误';
-                break;
-              case 3:
-                errorMessage = '参数错误';
-                break;
-              case 4:
-                errorMessage = '用户已存在';
-                break;
-              case 5:
-                errorMessage = '用户不存在';
-                break;
-              case 6:
-                errorMessage = '密码错误';
-                break;
-              case 7:
-                errorMessage = '无权限访问';
-                break;
-              case 8:
-                errorMessage = '班级错误';
-                break;
-              case 9:
-                errorMessage = '性别错误';
-                break;
-              case 10:
-                errorMessage = '用户名已存在';
-                break;
-              case 11:
-                errorMessage = '邮箱格式错误';
-                break;
-              case 12:
-                errorMessage = '真实姓名错误';
-                break;
-              case 13:
-                errorMessage = '学校信息错误';
-                break;
-              case 14:
-                errorMessage = '密码强度不够';
-                break;
-              default:
-                errorMessage = '未知错误';
-            }
-            ElMessage({
-              message: errorMessage,
-              type: 'error',
-              duration: 3000
-            });
-          })
+      .then((res) => {
+        if (res.data.status === 0) {
+          //状态码为0，提交成功，关闭当前对话框
+          dialogVisible.value = false;
+          reload();
+          ElMessage({
+            message: "用户添加完毕",
+            type: "success",
+          });
+        }
+      })
+      .catch((e) => {
+        let errorMessage = "登录失败";
+        switch (e.response.data.status) {
+          case 1:
+            errorMessage = "内部错误";
+            break;
+          case 3:
+            errorMessage = "参数错误";
+            break;
+          case 4:
+            errorMessage = "用户已存在";
+            break;
+          case 5:
+            errorMessage = "用户不存在";
+            break;
+          case 7:
+            errorMessage = "无权限访问";
+            break;
+          case 9:
+            errorMessage = "性别错误";
+            break;
+          case 10:
+            errorMessage = "用户名已存在";
+            break;
+          case 11:
+            errorMessage = "邮箱格式错误";
+            break;
+          case 12:
+            errorMessage = "真实姓名错误";
+            break;
+          case 13:
+            errorMessage = "学校信息错误";
+            break;
+          case 14:
+            errorMessage = "密码强度不够";
+            break;
+          default:
+            errorMessage = "未知错误";
+        }
+        ElMessage({
+          message: errorMessage,
+          type: "error",
+          duration: 3000,
+        });
+      });
   } else {
     //@ 在此处调用修改学生参数的接口
     // console.log('用户名',(newUserForm.value.name))
     await editStuInfo(
-        newUserForm.value.id,
-        newUserForm.value.name,
-        // newUserForm.value.password,
-        newUserForm.value.email,
-        newUserForm.value.realName,
-        newUserForm.value.userSchoollD,
-        newUserForm.value.schoolCode,
-        newUserForm.value.class,
-        newUserForm.value.sex
-    ).then((res) => {
-      if (res.data.status == 0) {
-        dialogVisible.value = false;
-        reload()
+      newUserForm.value.id,
+      newUserForm.value.name,
+      // newUserForm.value.password,
+      newUserForm.value.email,
+      newUserForm.value.realName,
+      newUserForm.value.userSchoollD,
+      newUserForm.value.schoolCode,
+      newUserForm.value.class,
+      newUserForm.value.sex,
+    )
+      .then((res) => {
+        if (res.data.status == 0) {
+          dialogVisible.value = false;
+          reload();
+          ElMessage({
+            message: "编辑成功",
+            type: "success",
+          });
+        }
+      })
+      .catch((e) => {
+        let errorMessage = "登录失败";
+        switch (e.response.data.status) {
+          case 1:
+            errorMessage = "内部错误";
+            break;
+          case 3:
+            errorMessage = "参数错误";
+            break;
+          case 5:
+            errorMessage = "用户不存在";
+            break;
+          case 7:
+            errorMessage = "无权限访问";
+            break;
+          case 8:
+            errorMessage = "班级错误";
+            break;
+          case 9:
+            errorMessage = "性别错误";
+            break;
+          case 10:
+            errorMessage = "用户名已存在";
+            break;
+          case 11:
+            errorMessage = "邮箱格式错误";
+            break;
+          case 12:
+            errorMessage = "真实姓名错误";
+            break;
+          case 13:
+            errorMessage = "学校信息错误";
+            break;
+          case 14:
+            errorMessage = "密码强度不够";
+            break;
+          default:
+            errorMessage = "未知错误";
+        }
         ElMessage({
-          message: '编辑成功',
-          type: 'success',
-        })
-      }
-    })
-    .catch(e => {
-            let errorMessage = '登录失败';
-            switch (e.response.data.status) {
-              case 1:
-                errorMessage = '内部错误';
-                break;
-              case 3:
-                errorMessage = '参数错误';
-                break;
-              case 5:
-                errorMessage = '用户不存在';
-                break;
-              case 7:
-                errorMessage = '无权限访问';
-                break;
-              case 8:
-                errorMessage = '班级错误';
-                break;
-              case 9:
-                errorMessage = '性别错误';
-                break;
-              case 10:
-                errorMessage = '用户名已存在';
-                break;
-              case 11:
-                errorMessage = '邮箱格式错误';
-                break;
-              case 12:
-                errorMessage = '真实姓名错误';
-                break;
-              case 13:
-                errorMessage = '学校信息错误';
-                break;
-              case 14:
-                errorMessage = '密码强度不够';
-                break;
-              default:
-                errorMessage = '未知错误';
-            }
-            ElMessage({
-              message: errorMessage,
-              type: 'error',
-              duration: 3000
-            });
-          })
+          message: errorMessage,
+          type: "error",
+          duration: 3000,
+        });
+      });
   }
 };
 
-
 //用于数据读取与展示
-
 </script>
 
 <template>
@@ -357,10 +338,10 @@ const handleSubmit = async () => {
     <!--    新增用户-->
     <el-button type="primary" @click="addStudent()">+新增</el-button>
     <el-dialog
-        v-model="dialogVisible"
-        :title="action == 'add' ? '添加学生' : '编辑学生'"
-        width="30%"
-        :before-close="handleClose"
+      v-model="dialogVisible"
+      :title="action == 'add' ? '添加学生' : '编辑学生'"
+      width="30%"
+      :before-close="handleClose"
     >
       <!--      学生注册组件表单-->
       <div class="form-container">
@@ -369,7 +350,11 @@ const handleSubmit = async () => {
             <el-input v-model="userForm.name" :disabled="IsDisabled"></el-input>
           </el-form-item>
           <el-form-item label="密码" prop="password" v-if="showPassword">
-            <el-input v-model="userForm.password" type="password" show-password/>
+            <el-input
+              v-model="userForm.password"
+              type="password"
+              show-password
+            />
           </el-form-item>
           <el-form-item label="邮箱" prop="email">
             <el-input v-model="userForm.email"></el-input>
@@ -378,10 +363,16 @@ const handleSubmit = async () => {
             <el-input v-model="userForm.realName"></el-input>
           </el-form-item>
           <el-form-item label="学生学号" prop="userSchoollD">
-            <el-input v-model="userForm.userSchoollD" :disabled="IsDisabled"></el-input>
+            <el-input
+              v-model="userForm.userSchoollD"
+              :disabled="IsDisabled"
+            ></el-input>
           </el-form-item>
           <el-form-item label="学校代码" prop="schoolCode">
-            <el-input v-model="userForm.schoolCode" :disabled="IsDisabled"></el-input>
+            <el-input
+              v-model="userForm.schoolCode"
+              :disabled="IsDisabled"
+            ></el-input>
           </el-form-item>
           <el-form-item label="学生班级" prop="class">
             <el-input v-model="userForm.class"></el-input>
@@ -394,7 +385,9 @@ const handleSubmit = async () => {
           </el-form-item>
           <el-form-item>
             <div class="button-container">
-              <el-button type="primary" @click="handleSubmit('userForm')">{{action == 'add' ? '添加' : '确认修改' }}</el-button>
+              <el-button type="primary" @click="handleSubmit('userForm')"
+                >{{ action == "add" ? "添加" : "确认修改" }}
+              </el-button>
               <el-button @click="dialogVisible = false">取消</el-button>
             </div>
           </el-form-item>
@@ -408,7 +401,7 @@ const handleSubmit = async () => {
         <el-input class="w-50 m-2" placeholder="输入姓名">
           <template #prefix>
             <el-icon class="el-input__icon">
-              <search/>
+              <search />
             </el-icon>
           </template>
         </el-input>
@@ -419,18 +412,24 @@ const handleSubmit = async () => {
     </el-form>
   </div>
   <div class="table">
-    <el-table :data="paginationStore.paginatedStuList" style="width: 100%" border>
+    <el-table
+      :data="paginationStore.paginatedStuList"
+      style="width: 100%"
+      border
+    >
       <el-table-column
-          v-for="item in tableLabel"
-          :key="item.prop"
-          :label="item.label"
-          :prop="item.prop"
-          :width="item.width ? item.width : 125"
-          table-layout="fix"
+        v-for="item in tableLabel"
+        :key="item.prop"
+        :label="item.label"
+        :prop="item.prop"
+        :width="item.width ? item.width : 125"
+        table-layout="fix"
       />
       <el-table-column fixed="right" label="操作" min-width="100">
         <template #default="scope">
-          <el-button size="default" @click="editStudent(scope.row)">编辑</el-button>
+          <el-button size="default" @click="editStudent(scope.row)"
+            >编辑
+          </el-button>
           <el-button type="danger" size="default">移出班级</el-button>
         </template>
       </el-table-column>
@@ -438,19 +437,16 @@ const handleSubmit = async () => {
   </div>
   <!-- 分页 -->
   <el-pagination
-      :page-size="paginationStore.pageSize"
-      background
-      default
-      layout="prev, pager, next"
-      :total="paginationStore.dataCount"
-      @current-change="changePage"
+    :page-size="paginationStore.pageSize"
+    background
+    default
+    layout="prev, pager, next"
+    :total="paginationStore.dataCount"
+    @current-change="changePage"
   />
-
 </template>
 
-
 <style scoped lang="less">
-
 .form-container {
   display: flex;
   justify-content: center;
