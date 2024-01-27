d734a68d4e6b5321edd6d7c93c861a79cb2d0f19
JoyfulLL <ljf13660117967@163.com>
2024-01-27 15:15:08 +0800

    修复教师身份下的搜索框错误

---
+++ src/mock/testMock.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+/**
+ * @file  testMock.vue
+ * @author ljf13
+ * @description
+ * @date 2024/1/26
+ */
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="less">
+
+</style>
